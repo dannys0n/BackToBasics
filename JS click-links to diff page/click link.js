a25e98b10668f04668f6179246418f04ff44b20e
dannys0n <72278864+dannys0n@users.noreply.github.com>
2020-12-27 01:54:32 -0800

    Add files via upload

---
+++ JS click-links to diff page/click link.js
@@ -0,0 +1,7 @@
+//creates a constant to pick this element
+const link = document.querySelector(".link")
+
+//links to new page
+link.addEventListener("click", () => {
+    window.location.href = "https://maryspaintings.netlify.app/index.html";
+});
--- click link.js
+++ click link.js
@@ -1,11 +1,36 @@
 const linkOne = document.querySelector("#picOne")
 const linkTwo = document.querySelector("#picTwo")
+const linkThree = document.querySelector("#picThree")
+const linkFour = document.querySelector("#picFour")
+const linkFive = document.querySelector("#picFive")
+const linkSix = document.querySelector("#picSix")
+const linkSeven = document.querySelector("#picSeven")
 
 
 linkOne.addEventListener("click", () => {
-    window.location.href = "https://github.com/";
+    window.location.href = "CSS Animation-blur background hover/index.html";
 });
 
 linkTwo.addEventListener("click", () => {
-    window.location.href = "https://www.google.com/";
+    window.location.href = "CSS structure-column 3 stack layout grids/index.html";
+});
+
+linkThree.addEventListener("click", () => {
+    window.location.href = "CSS structure-holy grail grids/index.html";
+});
+
+linkFour.addEventListener("click", () => {
+    window.location.href = "CSS Visual-image shadows/index.html";
+});
+
+linkFive.addEventListener("click", () => {
+    window.location.href = "CSS Visual-size & positions/index.html";
+});
+
+linkSix.addEventListener("click", () => {
+    window.location.href = "JS click-links to diff page/index.html";
+});
+
+linkSeven.addEventListener("click", () => {
+    window.location.href = "CSS Animation-slide transitions hover/index.html";
 });
